fb1dc1266be29efceba560fc67e946a4856fb07d
kasa021 <mkafa.k11@gmail.com>
2023-05-15 18:22:05 +0900

    feat: 四則演算を追加

--- 2/calc/main.js
+++ 2/calc/main.js
@@ -1,11 +1,21 @@
+
 document.addEventListener('DOMContentLoaded', () => {
     const num1 = document.getElementById('num1');
     const num2 = document.getElementById('num2');
     const result = document.getElementById('result');
+    const operator = document.getElementById('operator');
     const calcBtn = document.getElementById('calc-btn');
 
     calcBtn.addEventListener('click', () => {
-        const sum = parseInt(num1.value) + parseInt(num2.value);
-        result.textContent = sum;
+
+        if (operator.value === 'plus') {
+            result.textContent = parseInt(num1.value) + parseInt(num2.value);
+        } else if (operator.value === 'minus') {
+            result.textContent = parseInt(num1.value) - parseInt(num2.value);
+        } else if (operator.value === 'multiply') {
+            result.textContent = parseInt(num1.value) * parseInt(num2.value);
+        } else if (operator.value === 'divide') {
+            result.textContent = parseInt(num1.value) / parseInt(num2.value);
+        }   
     });
 });
