ba4cd13081001aa844c6e4f7a9c27eab61dc5136
n17y17 <n17y17@xmailer.be>
2023-05-15 19:58:25 +0900

    0で終了

--- 4/main.cpp
+++ 4/main.cpp
@@ -4,12 +4,17 @@ using namespace std;
 int add(int a, int b) {
     if(a<0 || b<0){
         cout << "ERROR" << endl;
-        exit(1);
+        exit(0);
     }
     return a + b;
 }
 
 int main() {
+    int a, b;
+    do{
+        cout << "数字を入力してください" << endl;
+        cin >> a >> b;
+    }while(a<0 || b<0);
     cout << add(3, -2) << endl;
     return 0;
 }
